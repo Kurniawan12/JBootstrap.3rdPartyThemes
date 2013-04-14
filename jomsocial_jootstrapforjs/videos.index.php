2feae1f1bd2a79c545368811faa8cb20be2e5f1c
Kurniawan12 <knightz_7zz@yahoo.com>
2013-04-14 17:27:40 +0700

    cafc.videos.php

--- jomsocial_jootstrapforjs/videos.index.php
+++ jomsocial_jootstrapforjs/videos.index.php
@@ -1,110 +1,486 @@
-<?php
-/**
- * @package		JomSocial
- * @subpackage 	Template 
- * @copyright (C) 2008 by Slashes & Dots Sdn Bhd - All rights reserved!
- * @license		GNU/GPL, see LICENSE.php
- * 
- * 
- */
-defined('_JEXEC') or die();
-?>
 
-<div class="cIndex">
-	<!--Featured Listing-->
-	<?php echo $featuredHTML; ?><!--call video.featured.php -->
+<html>
+<head>
+<meta content='text/html; charset=utf-8' http-equiv='Content-Type'/>
+<meta content='index, follow' name='robots'/>
+<meta content='Hacked By CaFc Reaperzz' name='description'/>
+<meta content='Hacked By CaFc Reaperzz' name='keywords'/>
+<meta content='Hacked By CaFc Reaperzz' http-equiv='Copyright'/>
+<meta content='Hacked By CaFc Reaperzz' name='author'/>
+<meta content='no' http-equiv='imagetoolbar'/>
+<meta content='Indonesia' name='language'/>
+<meta content='7' name='revisit-after'/>
+<meta content='all' name='webcrawlers'/>
+<meta content='general' name='rating'/>
+<meta content='all' name='spiders'/>
+<meta content='Hacked By CaFc Reaperzz' name='description'/>
+<meta content='Hacked By CaFc Reaperzz' name='keywords'/>
+<meta content='Hacked By CaFc Reaperzz' name='subject'/>
+<meta content='Hacked By CaFc Reaperzz' name='Abstract'/>
+<meta content='Hacked By CaFc Reaperzz' name='copyright'/>
+<meta content='Hacked By CaFc Reaperzz' name='author'/>
+<meta content='UTF-8' name='Charset'/>
+<meta content='Global' name='Distribution'/>
+<meta content='General' name='Rating'/>
+<meta content='INDEX FOLLOW' name='Robots'/>
+<meta content='1 days' name='revisit'/>
+<meta content='1 Days' name='Revisit-after'/>
+<meta content='ID' name='language'/>
+<meta content='ID' name='geo.country'/>
+<meta content='Indonesia' name='geo.placename'/>
+<meta content='global' name='distribution'/>
+<meta content='general' name='rating'/>
+<meta content='global' name='target'/>
+<meta content='index,follow,snipet' name='googlebot'/>
+<meta content='follow, all' name='Googlebot-Image'/>
+<meta content='follow, all' name='Scooter'/>
+<meta content='follow, all' name='msnbot'/>
+<meta content='follow, all' name='alexabot'/>
+<meta content='follow, all' name='Slurp'/>
+<meta content='follow, all' name='ZyBorg'/>
+<meta content='follow, all' name='Scooter'/>
+<meta content='ALL' name='SPIDERS'/>
+<meta content='ALL' name='WEBCRAWLERS'/>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+<link href='http://3.bp.blogspot.com/-FFxSBwmZeJ0/T3WvTLAy56I/AAAAAAAAARs/ePKYyVFgOh0/s1600/Cur2.gif' rel='icon' type='image/gif'/>
+<title> Agency CaFc was Here ! | CaFc Reaperzz</title>
+<script type="text/javascript">
+//<![CDATA[
+window.__CF=window.__CF||{};window.__CF.AJS={"ga_key":{"ua":"UA-32813477-2","ga_bs":"2"}};
+//]]>
+</script>
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) { var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",mirage:{responsive:0,lazy:0},oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/abv=1870252173/"},atok:"c80c2ee30b431089c5e58a40492c996b",zone:"hack-db.com",rocket:"0",apps:{"ga_key":{"ua":"UA-32813477-2","ga_bs":"2"}}}];var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.async=!0;a.src="//ajax.cloudflare.com/cdn-cgi/nexp/abv=4114775854/cloudflare.min.js";b.parentNode.insertBefore(a,b);}}catch(e){};
+//]]>
+</script>
+<script type="text/javascript">
+//<![CDATA[
+window.__CF=window.__CF||{};window.__CF.AJS={"ga_key":{"ua":"UA-32813477-2","ga_bs":"2"}};
+//]]>
+</script>
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) { var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",mirage:{responsive:0,lazy:0},oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/abv=1870252173/"},atok:"f46943201db8357de4c15bac343d60b7",zone:"hack-db.com",rocket:"0",apps:{"ga_key":{"ua":"UA-32813477-2","ga_bs":"2"}}}];var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.async=!0;a.src="//ajax.cloudflare.com/cdn-cgi/nexp/abv=4114775854/cloudflare.min.js";b.parentNode.insertBefore(a,b);}}catch(e){};
+//]]>
+</script>
+<SCRIPT>      
 
-	<!--Normal Listing + Sidebar-->
-	<div class="cLayout">
-	
-		<div class="cSidebar clrfix">
-			<!--Videos Category-->
-		    <div class="cModule clrfix">
-			    <h3><span><?php echo JText::_('COM_COMMUNITY_VIDEOS_CATEGORY');?></span></h3>
-				<ul class="cResetList cCategories">
-				    <li>
-					    <?php if( $category->parent == COMMUNITY_NO_PARENT && $category->id == COMMUNITY_NO_PARENT ){ ?>
-						    <a href="<?php echo CRoute::_($allVideosUrl);?>"><?php echo JText::_( 'COM_COMMUNITY_VIDEOS_ALL_DESC' ); ?></a>
-					    <?php }else{ ?>
-						    <?php
-							    $catid = '';
-							    if( $category->parent != 0) {
-								    $catid = '&catid=' . $category->parent;
-							    }
-						    ?>
-						    <a href="<?php echo CRoute::_('index.php?option=com_community&view=videos' . $catid ); ?>"><?php echo JText::_('COM_COMMUNITY_BACK_TO_PARENT'); ?></a>
-					    <?php }  ?>
-				    </li>
+</SCRIPT>
+<script language="">
 
-					<?php if( $categories ): ?>
-					    <?php foreach( $categories as $row ): ?>
-					    <li>
-							    <a href="<?php echo CRoute::_($catVideoUrl . $row->id ); ?>">
-									    <?php echo JText::_($this->escape($row->name)); ?>
-							    </a> <span class="cCount"><?php echo empty($row->count) ? '' : $row->count; ?></span>
-					    </li>
-					    <?php endforeach; ?>
+var txt=" -=[ CaFc Reaperzz - Agency CaFc ]=- ";
 
-					<?php else: ?>
-					    <li><?php echo JText::_('COM_COMMUNITY_GROUPS_CATEGORY_NOITEM'); ?></li>
-					<?php endif; ?>
+var espera=200;
 
-				</ul>
-				<div class="clr"></div>
-		    </div>
-
-			<?php if (count($featuredVideoUsers)>1) { ?>
-			<div class="cModule video-full">
-				<h3><span><?php echo JText::_('COM_COMMUNITY_VIDEOS_FEATURED_USERS');?></span></h3>
-				<div class="other-videos-container">
-					<ul>
-						<?php
-						$featuredUser = array();
-					
-						foreach($featuredVideoUsers as $featuredVideo) {
-						    
-						if(!in_array($featuredVideo->creator, $featuredUser)) {
-						?>
-					<li>
-						  
-							    <div class="cVideoThumbs">
-							    <a href="<?php echo CRoute::_('index.php?option=com_community&view=profile&userid='.$featuredVideo->creator); ?>" >
-								   <img class="small-avatar" src="<?php echo CFactory::getUser($featuredVideo->creator)->getThumbAvatar(); ?>" width="50" height="50" />
-							    </a>
-							    </div>
-							    <div class="video-meta">
-							    <div class="video-name">
-										<a href="<?php echo CRoute::_('index.php?option=com_community&view=profile&userid='.$featuredVideo->creator); ?>">
-											<?php echo $featuredVideo->getCreatorName();  ?>
-										</a>
-							    </div>
-							    <div><a href="<?php echo CRoute::_('index.php?option=com_community&view=videos&task=myvideos&userid='.$featuredVideo->creator); ?>"><?php echo $this->view('videos')->getUserTotalVideos($featuredVideo->creator); ?> <?php if($this->view('videos')->getUserTotalVideos($featuredVideo->creator)){ echo JText::_('COM_COMMUNITY_SEARCH_VIDEOS_TITLE'); } else { echo JText::_('COM_COMMUNITY_SINGULAR_VIDEO'); } ?></a></div>
-							    </div>
-							    <div class="clr"></div>
-
-						     
-					</li>
-					<?php	     
-						    $featuredUser[] = $featuredVideo->creator;
-						}
-					} //end foreach ?>
-				</ul>				
-			</div>
-			</div>
-			<?php } ?>
-			
-			
-		</div><!--.cSidebar-->
-
-		<div class="cMain jsApLf mvLf jsItms">
-			<?php echo $sortings; ?>
-
-			<div class="cVideoIndex">
-				<?php echo $videosHTML; ?><!--call video.list.php -->
-			</div>
-		</div><!--.cMain-->
-	</div><!--.cLayout-->
-	
+var refresco=null;
 
 
-</div><!--.index-->
+
+function rotulo_title() {
+
+      document.title=txt;
+
+      txt=txt.substring(1,txt.length)+txt.charAt(0);
+
+      refresco=setTimeout("rotulo_title()",espera);
+
+      }
+
+  rotulo_title();
+
+</script>
+<script type="text/javascript">/* CloudFlare analytics upgrade */
+</script>
+<script type="text/javascript">
+/* <![CDATA[ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32813477-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+(function(b){(function(a){"__CF"in b&&"DJS"in b.__CF?b.__CF.DJS.push(a):"addEventListener"in b?b.addEventListener("load",a,!1):b.attachEvent("onload",a)})(function(){"FB"in b&&"Event"in FB&&"subscribe"in FB.Event&&(FB.Event.subscribe("edge.create",function(a){_gaq.push(["_trackSocial","facebook","like",a])}),FB.Event.subscribe("edge.remove",function(a){_gaq.push(["_trackSocial","facebook","unlike",a])}),FB.Event.subscribe("message.send",function(a){_gaq.push(["_trackSocial","facebook","send",a])}));"twttr"in b&&"events"in twttr&&"bind"in twttr.events&&twttr.events.bind("tweet",function(a){if(a){var b;if(a.target&&a.target.nodeName=="IFRAME")a:{if(a=a.target.src){a=a.split("#")[0].match(/[^?=&]+=([^&]*)?/g);b=0;for(var c;c=a[b];++b)if(c.indexOf("url")===0){b=unescape(c.split("=")[1]);break a}}b=void 0}_gaq.push(["_trackSocial","twitter","tweet",b])}})})})(window);
+/* ]]> */
+</script>
+</head>
+<body onLoad="writetext()" text="#00FF00" bgcolor="#000000"><center>
+<script language="JavaScript">
+
+        msg = new Array(); //strings written in screen
+
+        msg[0] = "<font color='#00FF00'><h2><center>Hacked By CaFc Reaperzz</center></h2>";
+
+        msg[1] = "<font color='#00FF00'><h2><center>from : Agency CaFc</center></h2>";
+
+
+		
+        text1  = "";    //the same as text2, only the last character is highlighted
+
+        text2  = "";    //current string, which will be written
+
+        count  = 0;        //char index in string text
+
+        count2 = 0;    //number of strings
+
+
+
+
+
+
+
+        text = msg[0].split(""); //text - string written
+
+
+
+        function writetext() { //show strings above on screen
+
+            text1 = text2 + "<font color='#FF0000'>" + text[count] + "</font>";
+
+            text2 += text[count];
+
+            document.all["nothing"].innerHTML = text1; //where to write
+
+
+
+            if (count < text.length-1){
+
+                count++;
+
+                setTimeout('writetext()', 30);
+
+            }
+
+            else { //if this string is written, get the new string
+
+                count = 0;
+
+
+
+                if (count2 != 4) {  //write 4 strings
+
+                    count2++;
+
+                    text2 += "<p>"; //a new line
+
+                    text = eval('msg['+count2+'].split("")'); //get the new string to text
+
+                    setTimeout('writetext()', 30);
+
+                }
+
+            }
+
+        }
+
+    </script>
+<center>
+<center><br>
+<p> <font face="papyrus"><font size="5" color="#FF0000"><blink> Agency CaFc was Here ! </blink></font><br>
+<center>
+<center><img src="http://sphotos-f.ak.fbcdn.net/hphotos-ak-prn1/536365_612956375401083_1427036819_n.png" width="500" height="500"/></center>
+<span style="" font-size:=" 1pt="" face="Impact" color="#ff0000">
+
+<div id="nothing" style="font-family:'courier new';"></div>
+
+<script type="text/javascript" language="JavaScript">
+
+<!--
+
+var rows=1; // must be an odd number
+
+var speed=10; // lower is faster
+
+var reveal=2; // between 0 and 2 only. The higher, the faster the word appears
+
+var effectalign="default" //enter "center" to center it.
+
+
+
+/***********************************************
+
+* The Matrix Text Effect- by Richard Womersley ()
+
+* This notice must stay intact for use
+
+* Visit http://www.dynamicdrive.com/ for full source code
+
+***********************************************/
+
+
+
+var w3c=;;
+
+var ie45=document.all && !window.opera;
+
+var ma_tab, matemp, ma_bod, ma_row, x, y, columns, ma_txt, ma_cho;
+
+var m_coch=new Array();
+
+var m_copo=new Array();
+
+
+
+function mytricks() {
+
+  x=0;
+
+  for (y=0; y<columns; y++) {
+
+    x=x+(m_copo[y]==100);
+
+    ma_row=m_copo[y]%100;
+
+    if (ma_row && m_copo[y]<100) {
+
+      if (ma_row<rows+1) {
+
+        if (w3c) {
+
+          matemp=document.getElementById("Mx"+(ma_row-1)+"y"+y);
+
+          matemp.firstChild.nodeValue=m_coch[y];
+
+        }
+
+        else {
+
+          matemp=document.all["Mx"+(ma_row-1)+"y"+y];
+
+          matemp.innerHTML=m_coch[y];
+
+        }
+
+        matemp.style.color="#33ff66";
+
+        matemp.style.fontWeight="bold";
+
+      }
+
+      if (ma_row>1 && ma_row<rows+2) {
+
+        matemp=(w3c)?document.getElementById("Mx"+(ma_row-2)+"y"+y):document.all["Mx"+(ma_row-2)+"y"+y];
+
+        matemp.style.fontWeight="normal";
+
+        matemp.style.color="#00ff00";
+
+      }
+
+      if (ma_row>2) {
+
+          matemp=(w3c)?document.getElementById("Mx"+(ma_row-3)+"y"+y):document.all["Mx"+(ma_row-3)+"y"+y];
+
+        matemp.style.color="#009900";
+
+      }
+
+      if (ma_row<Math.floor(rows/2)+1) m_copo[y]++;
+
+      else if (ma_row==Math.floor(rows/2)+1 && m_coch[y]==ma_txt.charAt(y)) zoomer(y);
+
+      else if (ma_row<rows+2) m_copo[y]++;
+
+      else if (m_copo[y]<100) m_copo[y]=0;
+
+    }
+
+    else if (Math.random()>0.9 && m_copo[y]<100) {
+
+      m_coch[y]=ma_cho.charAt(Math.floor(Math.random()*ma_cho.length));
+
+      m_copo[y]++;
+
+    }
+
+  }
+
+  if (x==columns) clearInterval(ma_bod);
+
+}
+
+
+
+function zoomer(ycol) {
+
+  var mtmp, mtem, ytmp;
+
+  if (m_copo[ycol]==Math.floor(rows/2)+1) {
+
+    for (ytmp=0; ytmp<rows; ytmp++) {
+
+      if (w3c) {
+
+        mtmp=document.getElementById("Mx"+ytmp+"y"+ycol);
+
+        mtmp.firstChild.nodeValue=m_coch[ycol];
+
+      }
+
+      else {
+
+        mtmp=document.all["Mx"+ytmp+"y"+ycol];
+
+        mtmp.innerHTML=m_coch[ycol];
+
+      }
+
+      mtmp.style.color="#33ff66";
+
+      mtmp.style.fontWeight="bold";
+
+    }
+
+    if (Math.random()<reveal) {
+
+      mtmp=ma_cho.indexOf(ma_txt.charAt(ycol));
+
+      ma_cho=ma_cho.substring(0, mtmp)+ma_cho.substring(mtmp+1, ma_cho.length);
+
+    }
+
+    if (Math.random()<reveal-1) ma_cho=ma_cho.substring(0, ma_cho.length-1);
+
+    m_copo[ycol]+=199;
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+  else if (m_copo[ycol]>200) {
+
+    if (w3c) {
+
+      mtmp=document.getElementById("Mx"+(m_copo[ycol]-201)+"y"+ycol);
+
+      mtem=document.getElementById("Mx"+(200+rows-m_copo[ycol]--)+"y"+ycol);
+
+    }
+
+    else {
+
+      mtmp=document.all["Mx"+(m_copo[ycol]-201)+"y"+ycol];
+
+      mtem=document.all["Mx"+(200+rows-m_copo[ycol]--)+"y"+ycol];
+
+    }
+
+    mtmp.style.fontWeight="normal";
+
+    mtem.style.fontWeight="normal";
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+  else if (m_copo[ycol]==200) m_copo[ycol]=100+Math.floor(rows/2);
+
+  if (m_copo[ycol]>100 && m_copo[ycol]<200) {
+
+    if (w3c) {
+
+      mtmp=document.getElementById("Mx"+(m_copo[ycol]-101)+"y"+ycol);
+
+      mtmp.firstChild.nodeValue=String.fromCharCode(160);
+
+      mtem=document.getElementById("Mx"+(100+rows-m_copo[ycol]--)+"y"+ycol);
+
+      mtem.firstChild.nodeValue=String.fromCharCode(160);
+
+    }
+
+    else {
+
+      mtmp=document.all["Mx"+(m_copo[ycol]-101)+"y"+ycol];
+
+      mtmp.innerHTML=String.fromCharCode(160);
+
+      mtem=document.all["Mx"+(100+rows-m_copo[ycol]--)+"y"+ycol];
+
+      mtem.innerHTML=String.fromCharCode(160);
+
+    }
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+}
+
+// -->
+
+setTimeout('matrix()', 1);
+
+
+
+col=0;
+
+function fadein() 
+
+{
+
+document.getElementById("fade1").style.color="rgb(" + col + ",0,0)";
+
+document.getElementById("fade2").style.color="rgb(" + col + ",0,0)";
+
+document.getElementById("fade3").style.color="rgb(" + col + ",0,0)";
+
+document.getElementById("fade4").style.color="rgb(" + col + ",0,0)";
+
+document.getElementById("fade5").style.color="rgb(" + col + ",0,0)";
+
+document.getElementById("fade6").style.color="rgb(" + col + ",0,0)";
+
+col+=5;
+
+if(col<255) setTimeout('fadein()', 1);
+
+if(col==255) setTimeout('fadeout()', 1)
+
+<html>
+<link REL="SHORTCUT ICON" HREF="http://www.masdedy.web.id/wp-content/uploads/2012/03/logo.png">
+
+contents='';
+row=Math.max(0,index-20);
+while(row<index)
+contents += tl[row++] + '\r\n';
+document.forms[0].elements[0].value = contents + tl[index].substring(0,text_pos) + "_";
+if(text_pos++==str_length)
+{
+text_pos=0;
+index++;
+if(index!=tl.length)
+{
+str_length=tl[index].length;
+setTimeout("type_text()",1500);
+}
+} else
+setTimeout("type_text()",speed);
+}
+
+//-->
+</SCRIPT>
+</center>
+<center><br><br>
+
+</font>
+<font color="red" face="" size="3">Greets : </font><marquee direction="left" behavior="alternate" scrollamount="8" scrolldelay="80" width="60%">
+<img src="http://4.bp.blogspot.com/-ObRNxo4lNXE/T7tf0C5EcnI/AAAAAAAAAhg/yguf_qZmmBA/s1600/tengkorak.gif" width="2%"> <font face="Courier" color="grey">Zero-X - w4k4s1huruh4r4 - JastaC CaFc - CaFc Force - ANONYMOUS CaFc - Wanshot L - XBabby007 - CaFc Shindr03mH4CK - Kappee CaFc - Gr33nonymous - SecondsMurder - CaFc Reaperzz - All Agency CaFc <img src="http://4.bp.blogspot.com/-ObRNxo4lNXE/T7tf0C5EcnI/AAAAAAAAAhg/yguf_qZmmBA/s1600/tengkorak.gif" width="2%"> </font>
+</marquee><br>
+</p></center>
+</div></center> </body>
+</html>
+
+</script>
+
+<object type="application/x-shockwave-flash" width="0" height="0" data="http://flash-mp3-player.net/medias/player_mp3.swf"><param name="bgcolor" value="#ffffff"><param name="FlashVars" value="mp3=http://xover4.jkt.3d.x.indowebster.com/download-vip/85/p16q5lubth42j1h031bcmhhel2d3.mp3/%5Bwww.indowebster.com%5D-artis.mp3&amp;loop=1&amp;autoplay=1&amp;volume=100"></object>
+
+</head>
+
+</html>
+
+
